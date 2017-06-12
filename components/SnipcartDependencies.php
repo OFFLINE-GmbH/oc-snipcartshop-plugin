f92ef28aabffa3a30c0e51fa51f08eacfe2f69ba
Tobias Kündig <tobias@offline.swiss>
2017-06-12 15:54:38 +0200

    Added show_continue_shopping and split_firstname_and_lastname options

--- components/SnipcartDependencies.php
+++ components/SnipcartDependencies.php
@@ -14,13 +14,21 @@ class SnipcartDependencies extends ComponentBase
      * @var string
      */
     public $apiKey = '';
-
     /**
      * Opens the cart after adding a product.
      * @var boolean
      */
     public $autoPop = true;
-
+    /**
+     * Show an option to continue shopping.
+     * @var boolean
+     */
+    public $showContinueShopping = false;
+    /**
+     * Use separate fields for First name and Last name.
+     * @var boolean
+     */
+    public $splitFirstNameAndLastName = false;
     /**
      * Include jQuery from code.query.com.
      * @var boolean
@@ -58,6 +66,8 @@ class SnipcartDependencies extends ComponentBase
     {
         $this->setVar('apiKey', ApiSettings::get('public_api_key', ''));
         $this->setVar('autoPop', GeneralSettings::get('auto_pop', true));
+        $this->setVar('showContinueShopping', GeneralSettings::get('show_continue_shopping', false));
+        $this->setVar('splitFirstNameAndLastName', GeneralSettings::get('split_firstname_and_lastname', false));
         $this->setVar('activeCurrency', CurrencySettings::activeCurrency());
         $this->setVar('includeJquery', $this->property('includeJquery'));
     }
